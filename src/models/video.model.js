1dc26a2ef07521187abe415f8b64d4915e84c8c3
Gurdeep1845 <111561287+Gurdeep1845@users.noreply.github.com>
2024-06-20 15:14:26 +0500

    change

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -61,5 +61,13 @@ owner : {
 
 videoSchema.plugin(mongooseAggregatePAginate)
 
+// Let's say you have a list of 100 videos, and you want to see 10 videos at a time, like pages in a book. With the mongoose-aggregate-paginate-v2 plugin, you can easily get the first 10 videos, then the next 10, and so on, until you've seen all 100 videos.
+
+// Here's a simple example:
+
+// First Page (Videos 1-10):
+
+// Video.aggregatePaginate(aggregateQuery, { page: 1, limit: 10 })
+
 
 export const Video = mongoose.model("Video",videoSchema)
